8d025cb0de7f5d5d1d917bd2dce9d5f9f29f6ffb
jerryshew <jerryshew@gmail.com>
2016-11-13 14:55:10 +0800

    intro

---
+++ src/page/StartPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Hightlight from "react-highlight"
+import {CN, TitleBlock} from '../util/tools';
+import {NS, COLORS} from '../constant';
+
+
+export class StartPage extends Component {
+    render() {
+        return (
+            <section>
+                {TitleBlock('快速上手', '本节内容主要讲如何快速使用 React UIKit')}
+
+                <h4>引入组件</h4>
+                <Hightlight>
+                    {`import {DropDown, Pagination} from 'react-uikit'`}
+                </Hightlight>
+                <br/>
+
+                <h4>引入样式</h4>
+                <Hightlight>
+                    {`import styles from 'react-uikit/less/main.less'`}
+                </Hightlight>
+                <br/>
+
+                <h4>使用组件</h4>
+                <Hightlight>
+                    {`<Pagination />`}
+                </Hightlight>
+                <Hightlight>
+                    {`<Pagination current={3} />`}
+                </Hightlight>
+                <Hightlight>
+{`<RadioGroup value="a">
+    <Radio value="a">apple</Radio>
+    <Radio value="b">banana</Radio>
+    <Radio value="c">cat</Radio>
+</RadioGroup>
+`}
+                </Hightlight>
+            </section>
+
+        );
+    }
+}
