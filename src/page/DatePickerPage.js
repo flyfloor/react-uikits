a91c030daff1940f3a53ec3e41090bae0f0bd235
jerryshew <jerryshew@gmail.com>
2017-06-29 15:26:57 +0800

    datetime picker position style

--- src/page/DatePickerPage.js
+++ src/page/DatePickerPage.js
@@ -82,6 +82,7 @@ export class DatePickerPage extends Component {
                     {`<DatePicker type="year" onChange={onChangeFunction}/>`}
                 </CodeView>
                 <br/>
+                
                 <h4>日期选择位置</h4>
                 <CodeView component={<div>
                     <DatePicker position="top" onChange={value => {}}/>
@@ -146,6 +147,13 @@ export class DatePickerPage extends Component {
                             <td>函数(日期对象)</td>
                             <td>{`onChange(date){}`}</td>
                             <td>是</td>
+                        </tr>
+                        <tr>
+                            <td>position</td>
+                            <td>展开位置</td>
+                            <td>'left', 'right', 'top', 'bottom' 中的一个</td>
+                            <td>bottom</td>
+                            <td>否</td>
                         </tr>
                         <tr>
                             <td>begin</td>
